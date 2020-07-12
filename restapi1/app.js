7f7769641a7880471a6c1c55510d34ab5b196269
ikikika <iki.jito@gmail.com>
2020-07-12 22:17:41 +0800

    sample middleware

--- restapi1/app.js
+++ restapi1/app.js
@@ -2,9 +2,21 @@ const express = require("express");
 
 const app = express();
 
+// Middlewares
+// Functions that execute when routes are hit
+// Kinda similiar to controller@function
+
+app.use("/posts", () => {
+  console.log("This is a middleware");
+});
+
 // Routes
 app.get("/", (req, res) => {
   res.send("Hello world");
+});
+
+app.get("/post", (req, res) => {
+  res.send("Hello post");
 });
 
 // How to start listening/boot up the server?
