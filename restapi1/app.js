eaa25e84cd339964428cf4414dbcb9ed488a630d
ikikika <iki.jito@gmail.com>
2020-07-12 22:44:52 +0800

    connect to mongodb

--- restapi1/app.js
+++ restapi1/app.js
@@ -1,11 +1,14 @@
 const express = require("express");
-
 const app = express();
+const mongoose = require("mongoose");
+require("dotenv/config");
 
 // Middlewares
 // Functions that execute when routes are hit
 // Kinda similiar to controller@function
 
+// 1st argument must be a valid route
+// 2nd argument is the function
 app.use("/posts", () => {
   console.log("This is a middleware");
 });
@@ -18,6 +21,15 @@ app.get("/", (req, res) => {
 app.get("/post", (req, res) => {
   res.send("Hello post");
 });
+
+// Connect to DB
+mongoose.connect(
+  process.env.DB_CONNECTION,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("Connected to DB");
+  }
+);
 
 // How to start listening/boot up the server?
 app.listen(3000);
