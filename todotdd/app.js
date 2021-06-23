8da4ba4bdc8553ce343ae4b6fd3e217462956050
ikikika <iki.jito@gmail.com>
2021-06-24 00:29:56 +0800

    use supertest for integration tests

--- todotdd/app.js
+++ todotdd/app.js
@@ -1,11 +1,17 @@
 const express = require("express");
-
+const todoRoutes = require("./routes/todo.routes");
 const app = express();
+
+app.use(express.json);
+
+app.use("/todos", todoRoutes);
 
 app.get("/", (req, res) => {
   res.send("hello world");
 });
 
-app.listen(3000, () => {
-  console.log("server is running");
-});
+// app.listen(3000, () => {
+//   console.log("server is running");
+// });
+
+module.exports = app;
